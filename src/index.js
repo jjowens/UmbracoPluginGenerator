225bc579e7d1a67ab848d559e8fa2396f465ac43
= <=>
2021-03-22 16:24:14 +0000

    Inital script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-module.exports.UmbracoPluginGenerator = UmbracoPluginGenerator;
+const fs = require('fs');
 
-function UmbracoPluginGenerator(pluginName) {
-    console.log("PlugName:" + pluginName);
-}
+var myNamespace = function UmbracoPluginGenerator() {
+    
+    function GeneratePlugin(pluginConfig) {
+        console.log({pluginConfig});
+
+        let fileName = "/data.test.json";
+        let filePath = pluginConfig.projectpath + fileName; 
+        let jsonStr = JSON.stringify(pluginConfig);
+
+        writeToFile(filePath, jsonStr);
+
+
+        
+
+    }
+
+    function writeToFile(filePath, data) {
+        fs.writeFile(filePath, data, function (err) {
+            if (err) return console.log(err);
+            console.log('Generic Exported Data');
+        });
+    }
+
+    return {
+        GeneratePlugin: GeneratePlugin
+    }
+
+}();
+
+module.exports.UmbracoPluginGenerator = exports = myNamespace;
